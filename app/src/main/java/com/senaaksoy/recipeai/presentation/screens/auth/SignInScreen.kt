43a0e6ecc951ac61542e1c5b4b49493a87ec3d42
senajpeg <sena04176@gmail.com>
2025-11-06 17:13:27 +0300

    updated

--- app/src/main/java/com/senaaksoy/recipeai/presentation/screens/auth/SignInScreen.kt
+++ app/src/main/java/com/senaaksoy/recipeai/presentation/screens/auth/SignInScreen.kt
@@ -1,13 +1,186 @@
 package com.senaaksoy.recipeai.presentation.screens.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.senaaksoy.recipeai.R
+import com.senaaksoy.recipeai.components.EditTextField
 
 @Composable
-fun SignInScreen(
-    navController: NavController,
-    modifier: Modifier=Modifier
-){
+fun SignInScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFF5D10A2),
+                        Color(0xFF6257E7)
+                    )
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 36.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            // Logo
+            Icon(
+                painter = painterResource(R.drawable.recipeai_logo),
+                tint = Color.Unspecified,
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(0.6f)
+            )
 
+            // Email
+            EditTextField(
+                value = "",
+                onValueChange = {},
+                label = R.string.email,
+                keyboardOptions = androidx.compose.foundation.text.KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Email
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = null,
+                        tint = Color(0xFFcfccf0)
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(focusedLabelColor = Color.White)
+            )
+
+            // Password
+            EditTextField(
+                value = "",
+                onValueChange = {},
+                label = R.string.sifre,
+                keyboardOptions = androidx.compose.foundation.text.KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Password
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = null,
+                        tint = Color(0xFFcfccf0)
+                    )
+                },
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.VisibilityOff,
+                        contentDescription = null
+                    )
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                colors = OutlinedTextFieldDefaults.colors(focusedLabelColor = Color.White)
+            )
+
+            // Forgot password
+            Text(
+                text = stringResource(R.string.sifremi_unuttum),
+                color = Color(0xFFaea0e4),
+                modifier = Modifier
+                    .widthIn(max = 280.dp)
+                    .fillMaxWidth()
+                    .clickable { },
+                textAlign = TextAlign.End
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Login button
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .widthIn(max = 350.dp)
+                    .fillMaxWidth(0.6f)
+                    .clip(shape = RoundedCornerShape(12.dp))
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                Color(0xFFA065E3),
+                                Color(0xFF5E8BCB)
+                            )
+                        )
+                    ),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+            ) {
+                Text(text = stringResource(R.string.giris_yap))
+            }
+
+            // Google button
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .widthIn(max = 350.dp)
+                    .fillMaxWidth(0.6f)
+                    .clip(shape = RoundedCornerShape(12.dp)),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary
+                )
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.google),
+                    contentDescription = null,
+                    tint = Color.Unspecified,
+                    modifier = Modifier
+                        .width(18.dp)
+                        .height(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = stringResource(R.string.google_ile_devam_et))
+            }
+
+            // Sign up link
+            TextButton (onClick = { }) {
+                Text(
+                    text = stringResource(R.string.have_an_account_sign_up),
+                    color = Color(0xFFaea0e4)
+                )
+            }
+        }
+    }
 }
